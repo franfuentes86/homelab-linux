d6a3c24fabf36a03cadfae05682ba04ade3af66a
Fran Fuentes <142716269+franfuentes86@users.noreply.github.com>
2026-08-09 10:40:00 +0200

    Add system information script

--- scripts/system-info.sh
+++ scripts/system-info.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+echo "Hostname:"
+hostname
+
+echo
+
+echo "IP Address:"
+hostname -I
+
+echo
+
+echo "Memory:"
+free -h
+
+echo
+
+echo "Disk:"
+df -h
+
+echo
+
+echo "Uptime:"
+uptime
